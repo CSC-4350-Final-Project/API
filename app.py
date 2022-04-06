01ca9f1be006ea4388db5079ef33eae24dc35aa8
Chris Mai <cmai2@student.gsu.edu>
2022-04-06 03:01:19 -0400

    fixing Pylint errors

--- app.py
+++ app.py
@@ -1,73 +1,73 @@
-# pylint: disable=import-error
-# pylint: disable=unused-import
-import unittest
+# pylint: disable=no-member
+"""Main app"""
 import os
 import flask
-from flask import Flask, render_template, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, redirect, flash, request
 from flask_login import login_required, current_user, login_user, logout_user
 from dotenv import find_dotenv, load_dotenv
-from .models import db, User, login
+from models import db, User, loggingIn
 
 load_dotenv(find_dotenv())
 app = flask.Flask(__name__)
 
-db = SQLAlchemy(app, session_options={"autocommit": True})
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_POOL_SIZE"] = 100
 
-login.init_app(app)
-login.login_view = "login"
-
+loggingIn.init_app(app)
+loggingIn.login_view = "login"
 
 db.init_app(app)
 with app.app_context():
     db.create_all()
-
 
 # ROUTES
 
 # Landing
 @app.route("/")
 def landing():
+    """This page contains an app overview and Login/Register buttons"""
     return render_template("landing.html")
 
 
 # Login
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    # if user is already registered, take them to main page
+    """If user is already registered, take them to main page"""
     if current_user.is_authenticated:
         return redirect("/main")
 
-    if flask.request.method == "POST":
-        email = flask.request.form["email"]
+    if request.method == "POST":
+        email = request.form["email"]
         user = User.query.filter_by(email=email).first()
-        if user is not None and user.check_password(flask.request.form["password"]):
+        if user is not None and user.check_password(request.form["password"]):
             login_user(user)
             return redirect("/main")
+        if user is None:
+            flash("Not registered. Please register below.")
     return render_template("login.html")
 
 
 # Logout
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
+    """Logout the user"""
     logout_user()
-    return redirect(url_for("landing"))
+    return redirect("/")
 
 
 # Register
 @app.route("/register", methods=["POST", "GET"])
 def register():
+    """Register the email to the database"""
     if current_user.is_authenticated:
         return redirect("/main")
 
-    if flask.request.method == "POST":
-        email = flask.request.form["email"]
-        username = flask.request.form["username"]
-        password = flask.request.form["password"]
+    if request.method == "POST":
+        email = request.form["email"]
+        username = request.form["username"]
+        password = request.form["password"]
 
         if User.query.filter_by(email=email).first():
             flash("Email used. Try another.")
@@ -77,6 +77,7 @@ def register():
             db.session.add(user)
             db.session.commit()
             return redirect("/login")
+
     return render_template("register.html")
 
 
@@ -84,6 +85,7 @@ def register():
 @app.route("/main")
 @login_required
 def main():
+    """App's main page"""
     return render_template("main.html")
 
 
--- models.py
+++ models.py
@@ -1,29 +1,33 @@
 """Schemas for our database"""
 # pylint: disable=no-member
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from flask_login import LoginManager
 
+loggingIn = LoginManager()
 db = SQLAlchemy()
-login = LoginManager()
-
 
 # Models
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
+    """User table"""
+
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
-    username = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100))
     password_hash = db.Column(db.String(500))
 
     def set_password(self, password):
+        """Create a unique password hash for user's passwords"""
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        """check a new hashed against the created hash"""
         return check_password_hash(self.password_hash, password)
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+@loggingIn.user_loader
+def load_user(user_id):
+    """Load the user based on their primary id"""
+    return User.query.get(int(user_id))
