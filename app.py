92e706f399d35f20249a2288e5843f11abb29470
Melias96 <77294683+Melias96@users.noreply.github.com>
2022-04-08 14:21:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-"""Final Project"""
-import os
 import flask
-from flask_cors import CORS
-from dotenv import find_dotenv, load_dotenv
-from models import db
+import requests
+import os
+# pyright: reportMissingImports=false
+# pyright: reportMissingModuleSource=false
+from TM import get_event_data
+
 
 app = flask.Flask(__name__)
-CORS(app)
+app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
 
-load_dotenv(find_dotenv())
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+@app.route("/")
+def index():
+    data= get_event_data()
+    return flask.render_template(
+        "Homepage.html",
+        name=data['name'],
+        date=data['date'],
+        url=data['url'],
+    )
 
-db.init_app(app)
-with app.app_context():
-    db.create_all()
-
-# routes go here
-
-if __name__ == "__main__":
-    PORT = int(os.getenv("PORT", "4000"))
-    HOST = os.getenv("IP", "0.0.0.0")
-
-    app.run(debug=os.getenv("DEBUG"), host=HOST, port=PORT)
+app.run(
+    debug=True
+)
