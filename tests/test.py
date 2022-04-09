bb5feb02e8f2a5682f1492579b9be615eb92ed1c
Bingyi Xie <kbxxy@Bingyis-MacBook-Pro.local>
2022-04-08 23:18:49 -0400

    pylint check

--- tests/test.py
+++ tests/test.py
@@ -1,30 +1,39 @@
-import sys
+"""
+unit test
+"""
 import json
+import unittest
+import sys
 
 sys.path.append("../")
-
-import unittest
 from app import app
 
 
+sys.path.append("../")
+
+
 class BasicTestCase(unittest.TestCase):
+    "class"
+
     def test_search(self):
+        "test search"
         tester = app.test_client(self)
         response = tester.get("/search", content_type="json")
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.get_data())
-        postalCode = data["_embedded"]["events"][0]["_embedded"]["venues"][0][
+        postal_code = data["_embedded"]["events"][0]["_embedded"]["venues"][0][
             "postalCode"
         ]
-        self.assertEqual(postalCode, "30303")
+        self.assertEqual(postal_code, "30303")
 
     def test_event_detail(self):
+        "test detail"
         tester = app.test_client(self)
         response = tester.get("/event_detail/vvG1zZpmTbud8h", content_type="json")
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.get_data())
-        id = data["id"]
-        self.assertEqual(id, "vvG1zZpmTbud8h")
+        event_id = data["id"]
+        self.assertEqual(event_id, "vvG1zZpmTbud8h")
 
 
 if __name__ == "__main__":
